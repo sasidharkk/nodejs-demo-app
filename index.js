4069106519c38bda92987629b12ffed5108e669f
sasidharkk <sasidhark842gmail.com>
2025-10-21 09:59:28 +0530

    Initial commit - Node app + Dockerfile

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Pipeline!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
+
